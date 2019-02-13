3f277b7858fad06239c2622a05e4f4d50710f77c
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2019-02-12 20:13:43 -0600

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shlly
+
 A simple (and extremely limited) UNIX shell implemented in Python
---
+++ src/main.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+
+from __future__ import print_function
+
+import os
+import sys
+import signal
+
+PS1 = "%s :: ~> " % os.getcwd()
+
+
+def _keyboard_interrupt(signum, frame):
+    print("\n\nThanks for using shlly! Fork our project at https://github.com/marshalhayes/shlly.\n")
+    sys.exit(1)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        # exit on keyboard interrupt (ctrl + c)
+        signal.signal(signal.SIGINT, _keyboard_interrupt)
+
+        # collect input and trim trailing whitespace
+        command = raw_input(PS1).strip().split('\n')[0].split()
+        print(command)
