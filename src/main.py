1877eb55f6395709e03ed7a2dbb4a67797974939
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2019-02-25 17:19:04 -0600

    pushing few changes

--- src/main.py
+++ src/main.py
@@ -11,7 +11,19 @@ PS1 = "%s :: ~> " % os.getcwd()
 
 def _keyboard_interrupt(signum, frame):
     print("\n\nThanks for using shlly! Fork our project at https://github.com/marshalhayes/shlly.\n")
-    sys.exit(1)
+    sys.exit(0)
+
+
+def parse_command(command):
+    """
+        Parse a command
+
+        command: a list of program and flags
+    """
+    prog = command[0]
+    if prog == "exit":
+        sys.exit(0)
+    return
 
 
 def main():
@@ -25,4 +37,4 @@ if __name__ == "__main__":
     while True:
         # collect input and trim trailing whitespace
         command = raw_input(PS1).strip().split('\n')[0].split()
-        print(command)
+        parse_command(command)
