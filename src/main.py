8c69c722b31972af9bc17bdd92bc7bb6d04e915c
arcsenenon <arcseneon@gmail.com>
2019-02-25 17:47:03 -0600

    Removed keyboard interrupt

--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
-#! /usr/bin/python
-
-from __future__ import print_function
+#! /usr/bin/python3
 
 import os
 import sys
 import signal
 
 PS1 = "%s :: ~> " % os.getcwd()
-
-
-def _keyboard_interrupt(signum, frame):
-    print("\n\nThanks for using shlly! Fork our project at https://github.com/marshalhayes/shlly.\n")
-    sys.exit(0)
-
 
 def parse_command(command):
     """
@@ -22,19 +14,17 @@ def parse_command(command):
     """
     prog = command[0]
     if prog == "exit":
+        print("\n\nThanks for using shlly! Fork our project at https://github.com/marshalhayes/shlly.\n")
         sys.exit(0)
     return
-
 
 def main():
     pass
 
 
 if __name__ == "__main__":
-    # exit on keyboard interrupt (ctrl + c)
-    signal.signal(signal.SIGINT, _keyboard_interrupt)
-
+    
     while True:
         # collect input and trim trailing whitespace
-        command = raw_input(PS1).strip().split('\n')[0].split()
+        command = input(PS1).strip().split('\n')[0].split()
         parse_command(command)
