e45e693356343ba61628f1cafdbdbb2f3fdb1788
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2019-02-25 19:59:28 -0600

    adding cd functionality and program exec calls

--- src/main.py
+++ src/main.py
@@ -6,13 +6,6 @@ import os
 import sys
 import time
 
-PS1 = "%s :: ~> " % os.getcwd()
-COMMANDS = {
-    "cd",
-    "pwd",
-    "stat",
-    "exit"
-}
 HISTORY = []
 
 
@@ -22,29 +15,37 @@ def update_history(command):
         @param: command: a list of program and flags
     """
     current_time = datetime.now().strftime("%H:%M")
-    str_command = "%s %s" % (current_time, "".join(command))
+    str_command = "%s %s" % (current_time, " ".join(command))
     HISTORY.append(str_command)
 
 
-def new_process(command):
+def new_process(prog, args):
+    """
+        Fork a new process and execute prog with arg
+        @param: prog: a string; the program (or path to program) to execute
+        @param: args: a list; the arguments to program
+    """
     # fork new process
     proc_id = os.fork()
 
     # replace memory space with user program
     if proc_id == 0:
         # replace memory space
-        os.execlp(command[0], command[0], command[1:])
+        os.execlp(prog, prog, *args)
     elif proc_id < 0:
-        print("ERROR: Fork failed!") # Fork failed
+        print("ERROR: Fork failed!")  # Fork failed
     else:
-        print(proc_id)
-        time.sleep(None)
+        time.sleep(0.01)
+
 
 def parse_command(command):
     """
         Parse a command
         @param: command: a list of program and flags
     """
+    if len(command) == 0:
+        return
+
     prog, args = command[0], command[1:]
     if prog == "exit":
         print("\n\nThanks for using shlly! Fork our project at https://github.com/marshalhayes/shlly.\n")
@@ -52,16 +53,35 @@ def parse_command(command):
     elif prog == "stat":
         for history in HISTORY:
             print(history)
-    return
+    elif prog == "cd":
+        # chdir sys call to change path
+        if args[0] == '-':
+            print("Not implemented yet")
+        else:
+            path = os.path.normpath(args[0])
+            os.chdir(path)
+    else:
+        try:
+            new_process(prog, args)
+        except FileNotFoundError:
+            print("ERROR: Program %s not found" % prog)
+            sys.exit(1)
 
 
 def main():
     pass
 
+
 if __name__ == "__main__":
     while True:
-        # collect input and trim trailing whitespace
-        command = input(PS1).strip().split('\n')[0].split()
-        new_process(command)
-        parse_command(command)
-        update_history(command)
+        try:
+            PS1 = "%s ::~> " % os.getcwd()  # prompt
+            # collect input and trim trailing whitespace
+            command = input(PS1).strip().split('\n')[0].split()
+            parse_command(command)
+            update_history(command)
+        except (KeyboardInterrupt, EOFError):
+            # On keyboard interrupt (ctrl-c and EOF (ctrl-d)), exit the program cleanly
+            print(
+                "\n\nThanks for using shlly! Fork our project at https://github.com/marshalhayes/shlly.\n")
+            sys.exit(0)
