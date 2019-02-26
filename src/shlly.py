8ea31e07c6297bf8ef3792ac738b960961b0ce74
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2019-02-26 00:01:12 -0600

    updating cd functionality to include cd with no path argument

--- src/shlly.py
+++ src/shlly.py
@@ -53,10 +53,16 @@ def parse_command(command):
         for history in HISTORY:
             print(history)
     elif prog == "cd":
-        # chdir sys call to change path
-        if args[0] == '-':
-            args[0] = PREV_DIR or os.getcwd()
-        path = os.path.normpath(args[0])
+        if args:
+            # chdir sys call to change path
+            if args[0] == '-':
+                args[0] = PREV_DIR or os.getcwd()
+            elif args[0][0] == '~':
+                # if the user uses ~ to specify home directory, find the
+                # home directory from the path variable $HOME
+                args[0] = args[0].replace('~', os.environ["HOME"])
+        # if no path is given as an argument to cd, change directory to $HOME
+        path = os.path.normpath(args[0] if args else os.environ["HOME"])
         os.chdir(path)
     else:
         try:
